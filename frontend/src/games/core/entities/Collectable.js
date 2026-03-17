a1f8c2dbc5ba481e2015c0122c0bd8d241e0ff6f
Lukas Stefanek <lstefane@student.42vienna.com>
2026-03-17 15:49:55 +0100

    feat: Add scaleUp Animation when collectable spawns

--- frontend/src/games/core/entities/Collectable.js
+++ frontend/src/games/core/entities/Collectable.js
@@ -9,13 +9,15 @@ export class Collectable {
 
     const pos = options.position || [0, 0, 0];
     const rotation = options.rotation || 0;
-    const scale = options.scale || [1, 1, 1];
+    const scale = options.scale || [0, 0, 0];
 
     this.model.position.set(...pos);
     this.model.rotation.set(0, rotation, 0);
-    this.model.scale.set(...scale);
+    this.model.scale.set(0, 0, 0);
+    //this.model.scale.set(...scale);
     this.model.updateMatrixWorld(true);
-    this.hidden = false;
+    this.targetScale = new THREE.Vector3(1, 1, 1);
+    this.new = true;
 
     if (this.animations && this.animations.length > 1) {
       this.mixer = new THREE.AnimationMixer(this.model);
@@ -29,11 +31,24 @@ export class Collectable {
       this.mixer.update(delta);
     }
 
+    if (this.new) {
+      this.scaleUp(delta)
+    }
+
     const distance = this.model.position.distanceTo(gPlayer.value.model.position);
     if (distance < 2.5) {
       console.log("collect coin!");
       gUser.value.coins += 1;
       this.destroy();
+    }
+  }
+
+  scaleUp(delta) {
+    if (this.model.scale.distanceTo(this.targetScale) < 0.01) {
+      this.model.scale.copy(this.targetScale);
+      this.new = false;
+    } else {
+      this.model.scale.lerp(this.targetScale, delta * 2.5);
     }
   }
 
