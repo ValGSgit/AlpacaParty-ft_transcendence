ed6f46aaab4b6b87e4831fc1a244436a35fd48e5
Lukas Stefanek <lstefane@student.42vienna.com>
2026-03-23 13:33:47 +0100

    fix: Attach collider correctly

--- frontend/src/games/core/createObjects.js
+++ frontend/src/games/core/createObjects.js
@@ -70,7 +70,7 @@ export async function createCollectable(
 ) {
   const { model, animations } = await getModel(path);
   const clone = SkeletonUtils.clone(model);
-  attachCollider(collectable.model);
+  attachCollider(clone);
 
   const collectable = new Collectable(clone, animations, { position, rotation, scale });
 
