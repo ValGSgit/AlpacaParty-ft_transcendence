7655c23ac5460bdd591249fd111e265a53253af3
Lukas Stefanek <lstefane@student.42vienna.com>
2026-03-25 11:43:36 +0100

    improve: Show FPS stats only in debug mode

--- frontend/src/games/core/FPSstats.js
+++ frontend/src/games/core/FPSstats.js
@@ -1,10 +1,13 @@
 import Stats from 'three/addons/libs/stats.module.js';
+import { CONST } from '../config/constants';
 
 export function initFPSstats(container) {
+    if (!CONST.DEBUG) return;
+
     const stats = new Stats();
 
     stats.dom.style.position = 'absolute';
-    stats.dom.style.top = '80px';
+    stats.dom.style.top = '120px';
     stats.dom.style.left = '20px';
     stats.dom.style.zIndex = '10';
 
@@ -16,6 +19,8 @@ export function initFPSstats(container) {
 }
 
 export function cleanupFPSstats(stats, container) {
+    if (!CONST.DEBUG) return;
+
     if (stats && stats.dom && container && container.contains(stats.dom)) {
         container.removeChild(stats.dom);
     }
