8d37fec3924a32806815e72043ad7094855b87a8
Ka Hou <fankahou@gmail.com>
2026-03-30 22:56:56 +0900

    opened up a commended out bug due to merge

--- frontend/src/games/Game.vue
+++ frontend/src/games/Game.vue
@@ -210,7 +210,7 @@ const { openEditMode, closeEditMode, openShopMenu, closeShopMenu, openAlpacaShop
 const { init, cleanup, onResize } = useGameEngine(gameContainer)
 const { increaseFarmSize } = useShop()
 const { buyItem } = itemShop()
-//const { isAuthenticated } = useAuthStore()
+const { isAuthenticated } = useAuthStore()
 const { cancelPlacement, deleteItem} =  useEditMode()
 const showLoginWarning = ref(false);
 const warningOff = () => {showLoginWarning.value = false;};
