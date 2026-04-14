e7a8de12e8905c161a8f613811d43659b378f9d4
Lukas Stefanek <Stefanek.Lukas@gmx.at>
2026-04-14 15:13:24 +0200

    feat: Add scaleUp functions to buildings at start

--- frontend/src/games/mini_games/alpacaRoad.js
+++ frontend/src/games/mini_games/alpacaRoad.js
@@ -14,8 +14,10 @@ import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js';
 
 const stripeLength = 10;
 const roadLength = 700;
-const roadOffset = 100;
-const roadSpeed = 50;
+const roadBack = -25;
+const roadOffset = roadLength / 2 + roadBack;
+const roadSpeed = 25;
+const startZ = roadLength + roadBack;
 
 let level = 0;
 let alivePlayers;
@@ -35,7 +37,7 @@ const activeObstacles = [];
 const buildingSelection = []; // Templates loaded from GLB
 const activeBuildings = [];   // Instances currently on the road
 const buildingDepth = 60;     // How "wide" the building is along the Z axis
-const roadSideOffset = 0;    // Distance from center of road to the buildings
+const buildingOffset = -28;    // Distance from center of road to the buildings
 const roadScene = [];
 let assetsLoaded = false;
 
@@ -44,7 +46,7 @@ const { spawnFloatingText } = useFloatingText();
 export async function initAlpacaRoad(playerCount, tempAlpacas) {
   level = 0;
   cleanupAlpacaRoad()
-  gUI.cameraMode = 2;
+  gUI.cameraMode = 0;
 
   await setupRoadScene(gScene.value);
   await loadAssets(gScene.value);
@@ -128,32 +130,33 @@ async function initPlayers(playerCount, tempAlpacas) {
 function initScenery() {
   if (buildingSelection.length === 0) return;
 
-  // Calculate how many buildings we need to cover the entire road length
   const numBuildings = Math.ceil(roadLength / buildingDepth) + 2; 
-  const startZ = -roadLength / 2 + roadOffset;
 
   for (let i = 0; i < numBuildings; i++) {
-    const zPos = startZ + (i * buildingDepth);
+    const zPos = i * buildingDepth;
 
-    // 1. Create Right Side Building
-    const bRight = buildingSelection[0].clone();
-    bRight.position.set(0, 0, zPos);
-    gScene.value.add(bRight);
-    activeBuildings.push(bRight);
+    // Right
+    const rBuild = buildingSelection[0].clone();
+    rBuild.position.set(buildingOffset, 0, zPos);
+    gScene.value.add(rBuild);
+    activeBuildings.push(rBuild);
+    rBuild.userData.targetScale = new THREE.Vector3(1, 1, 1);
 
-    // 2. Create Left Side Building (MIRRORED)
-    const bLeft = buildingSelection[0].clone();
-    bLeft.scale.x = -1;
-    bLeft.position.set(-0, 0, zPos);
-    gScene.value.add(bLeft);
-    activeBuildings.push(bLeft);
+    // Left
+    const lBuild = buildingSelection[0].clone();
+    lBuild.scale.x = -1;
+    lBuild.position.set(-buildingOffset, 0, zPos);
+    gScene.value.add(lBuild);
+    activeBuildings.push(lBuild);
+    lBuild.userData.targetScale = new THREE.Vector3(-1, 1, 1);
   }
 }
 
 function initRoadStripes() {
-  const numRows = 7;
+  const numRows = 8;
   const spacingZ = roadLength / numRows;
-  const startZ = (roadLength / 2 + roadOffset) - stripeLength / 2;
+
+  console.log(startZ);
 
   const stripeMat = new THREE.MeshStandardMaterial({ color: '#dddddd' });
   const stripe = PRIMITIVES.Box(1, 0.1, stripeLength, stripeMat);
@@ -289,33 +292,34 @@ function removeObstacle(obstacle, index){
 }
 
 function updateRoadScene(delta) {
-const movement = roadSpeed * delta;
-const backThreshold = -roadLength / 2 + roadOffset; // Where buildings "disappear"
-const teleportDistance = roadLength;    // How far forward they jump
+  const movement = roadSpeed * delta;
 
-// 1. Update Stripes & Sidewalks (Existing logic)
-for (let i = 1; i < roadScene.length; i++) {
-  const item = roadScene[i];
-  item.position.z -= movement;
-  if (item.position.z < backThreshold + roadOffset) {
-    item.position.z += teleportDistance;
+  for (let i = 1; i < roadScene.length; i++) {
+    const item = roadScene[i];
+    item.position.z -= movement;
+    if (item.position.z < roadBack) {
+      item.position.z = startZ;
+    }
   }
-}
 
-// 2. Update Buildings (The City Loop)
-for (let i = 0; i < activeBuildings.length; i++) {
-  const bld = activeBuildings[i];
-  bld.position.z -= movement;
+  for (let i = 0; i < activeBuildings.length; i++) {
+    const building = activeBuildings[i];
+    building.position.z -= movement;
 
-  // If the building passes the player and goes off-screen
-  if (bld.position.z < backThreshold) {
-    // ✅ Teleport to the front of the "conveyor belt"
-    bld.position.z += teleportDistance + 200;
-    
-    // Optional: Randomize which building model is used if you have multiple
-    // if (buildingSelection.length > 1) { swapModelLogic(); }
+    if (building.position.z < roadBack) {
+      building.position.z = startZ;
+      building.scale.set(0.1, 0.1, 0.1);
+      // Optional: Randomize which building model is used
+      // if (buildingSelection.length > 1) { swapModelLogic(); }
+    }
+    const target = building.userData.targetScale;
+    if (building.scale.distanceTo(target) < 0.01) {
+      building.scale.copy(building.userData.targetScale);
+    } else
+    {
+      building.scale.lerp(building.userData.targetScale, delta * 5);
+    }
   }
-}
 }
 
 function spinAlpacaUp(alpaca, delta) {
@@ -345,7 +349,7 @@ function checkAlpaca(alpaca) {
   const isColliding = checkCollisionWith(alpaca.model, activeObstacles);
   if (isColliding) {
     alpaca.isBeingHit = true;
-    //alpaca.hp--;
+    //alpaca.hp--; TODO
     spawnFloatingText(alpaca.model, '-💔', 'hearts');
     if (alpaca.hp === 0) {
       alpaca.isDead = true;
