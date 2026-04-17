97a413071d696f5c10fa30ff368a47c250a556cd
Lukas Stefanek <lstefane@student.42vienna.com>
2026-04-17 15:31:19 +0200

    refactor: Spawn in ring function

--- frontend/src/games/components/upgradeFarm.js
+++ frontend/src/games/components/upgradeFarm.js
@@ -1,12 +1,7 @@
-import * as THREE from 'three';
 import { CONST } from '../config/constants.js';
-import { createDecoration } from '../core/createObjects.js';
 import { shopItems } from '../core/entities/Item.js';
-import { gCollidables, gScene, gUser } from '../core/globals.js';
-import { getModel } from '../core/modelCache.js';
-import { attachCollider } from '../core/useCollider.js';
-import { usePhysics } from '../core/usePhysics.js';
-import { getRandomRot, getRandomScale } from '../utils/randomValues.js';
+import { gScene, gUser } from '../core/globals.js';
+import { spawnObjectRandomly } from '../utils/spawnRandomly.js';
 
 const UPGRADE_COST = [25, 50, 100, 250, 500];
 const HERDSIZE_COST = [5, 15, 25, 50, 100];
@@ -30,15 +25,15 @@ export function upgradeFarm() {
     floor.scale.z = CONST.FLOOR_RADIUS / CONST.BASE_RADIUS
 
     const grass = shopItems.find(item => item.name === 'Grass');
-    const newGrassGroup = await spawnGrassInRing(grass, 10);
+    const newGrassGroup = await spawnObjectRandomly(grass, 10, true);
     gScene.value.add(newGrassGroup);
 
     const stones = shopItems.find(item => item.name === 'Stones');
-    const stonesGroup = await spawnGrassInRing(stones, 3);
+    const stonesGroup = await spawnObjectRandomly(stones, 3, true);
     gScene.value.add(stonesGroup);
 
     const tree = shopItems.find(item => item.name === 'Tree');
-    const treeGroup = await spawnGrassInRing(tree, 2);
+    const treeGroup = await spawnObjectRandomly(tree, 2, true);
     gScene.value.add(treeGroup);
   }
   return { increaseFarmSize }
@@ -55,66 +50,4 @@ export function checkCoinsPrice(cost) {
     return false;
   }
   return true;
-}
-
-async function spawnGrassInRing(object, amount) {
-  const itemsGroup = new THREE.Group();
-  const itemsData = await getRandomPointInRing(object.path, amount, CONST.FLOOR_RADIUS - 5, CONST.FLOOR_RADIUS);
-
-  for (const data of itemsData) {
-    const item = await createDecoration(data.path, data.position, data.rotation, data.scale);
-    if (item && item.model) {
-      item.model.userData.cost = object.cost;
-    }
-    itemsGroup.add(item.model);
-  }
-  return itemsGroup;
-}
-
-async function getRandomPointInRing(path, amount, oldRadius, newRadius) {
-  const itemsData = [];
-  const { checkCollisionWith } = usePhysics();
-  const { model } = await getModel(path);
-  const dummy = model.clone();
-  attachCollider(dummy);
-
-  for (let i = 0; i < amount; i++) {
-    let isColliding = true;
-    let attempts = 0;
-    let pos = new THREE.Vector3();
-    let scale = getRandomScale();
-    let rot = getRandomRot();
-    dummy.scale.copy(scale);
-    dummy.rotation.y = rot;
-
-    while (isColliding && attempts < 100) {
-      const angle = Math.random() * Math.PI * 2;
-
-      // 2. Calculate the evenly distributed radius using the area math
-      const rInnerSq = oldRadius * oldRadius;
-      const rOuterSq = newRadius * newRadius;
-      const randomRadius = Math.sqrt(Math.random() * (rOuterSq - rInnerSq) + rInnerSq);
-
-      // 3. Convert the angle and radius back into X and Z coordinates
-      const x = Math.cos(angle) * randomRadius;
-      const z = Math.sin(angle) * randomRadius;
-      dummy.position.x = x;
-      dummy.position.z = z;
-      dummy.updateMatrixWorld(true);
-      isColliding = checkCollisionWith(dummy, gCollidables);
-      if (!isColliding) {
-        pos.set(x, 0, z);
-      }
-      attempts++;
-    }
-    if (!isColliding) {
-      itemsData.push({
-        path: path,
-        position: pos.toArray(),
-        rotation: rot,
-        scale: scale.toArray()
-      })
-    }
-  }
-  return itemsData;
 }
--- frontend/src/games/utils/spawnRandomly.js
+++ frontend/src/games/utils/spawnRandomly.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { CONST } from '../config/constants';
 import { createAlpaca, createCollectable, createDecoration, createItem } from "../core/createObjects";
 import { gCollidables } from '../core/globals';
 import { getModel } from '../core/modelCache';
@@ -6,9 +7,14 @@ import { attachCollider } from '../core/useCollider';
 import { usePhysics } from "../core/usePhysics";
 import { getRandomPos, getRandomRot, getRandomScale } from "./randomValues";
 
-export async function spawnObjectRandomly(object, amount) {
+export async function spawnObjectRandomly(object, amount, ring = false) {
   const itemsGroup = new THREE.Group();
-  const itemsData = await getValidRandomPos(object.path, amount);
+  let itemsData;
+  if (ring) {
+    itemsData = await getRandomPointInRing(object.path, amount);
+  } else {
+    itemsData = await getValidRandomPos(object.path, amount);
+  }
 
   for (const data of itemsData) {
     let item;
@@ -59,6 +65,56 @@ export async function getValidRandomPos(path, amount) {
       dummy.position.copy(pos);
       dummy.updateMatrixWorld(true);
       isColliding = checkCollisionWith(dummy, gCollidables);
+      attempts++;
+    }
+    if (!isColliding) {
+      itemsData.push({
+        path: path,
+        position: pos.toArray(),
+        rotation: rot,
+        scale: scale.toArray()
+      })
+    }
+  }
+  return itemsData;
+}
+
+async function getRandomPointInRing(path, amount) {
+  const oldRadius = CONST.FLOOR_RADIUS - 5;
+  const newRadius = CONST.FLOOR_RADIUS;
+  const itemsData = [];
+  const { checkCollisionWith } = usePhysics();
+  const { model } = await getModel(path);
+  const dummy = model.clone();
+  attachCollider(dummy);
+
+  for (let i = 0; i < amount; i++) {
+    let isColliding = true;
+    let attempts = 0;
+    let pos = new THREE.Vector3();
+    let scale = getRandomScale();
+    let rot = getRandomRot();
+    dummy.scale.copy(scale);
+    dummy.rotation.y = rot;
+
+    while (isColliding && attempts < 100) {
+      const angle = Math.random() * Math.PI * 2;
+
+      // Calculate the evenly distributed radius using the area math
+      const rInnerSq = oldRadius * oldRadius;
+      const rOuterSq = newRadius * newRadius;
+      const randomRadius = Math.sqrt(Math.random() * (rOuterSq - rInnerSq) + rInnerSq);
+
+      // Convert the angle and radius back into X and Z coordinates
+      const x = Math.cos(angle) * randomRadius;
+      const z = Math.sin(angle) * randomRadius;
+      dummy.position.x = x;
+      dummy.position.z = z;
+      dummy.updateMatrixWorld(true);
+      isColliding = checkCollisionWith(dummy, gCollidables);
+      if (!isColliding) {
+        pos.set(x, 0, z);
+      }
       attempts++;
     }
     if (!isColliding) {
