cf7766fb9947a28ef87f2c6ae085866e121d0f58
Ka Hou <fankahou@gmail.com>
2026-05-10 21:03:02 +0900

    added farm visiting

--- frontend/src/games/Game.vue
+++ frontend/src/games/Game.vue
@@ -86,10 +86,11 @@
     <div class="hud-container hud-right">
       <button class="hud-btn" @click="openGameMenu()" title="Mini Games">🕹️</button>
       <button v-if="!gMinigame.mode" class="hud-btn" @click="addDebugCoins()" title="DEBUG: Add Coins" style="background: #ffd700; color: #000;">🪙</button>
+      <button v-if="!gMinigame.mode" class="hud-btn" @click="openLobbyMenu(0)" title="Visit Farm">🏚️</button>
       <button v-if="!gMinigame.mode" class="hud-btn" @click="openShopMenu()" title="Shop">🛍️</button>
       <button v-if="!gMinigame.mode" class="hud-btn" @click="openEditMode()" title="Edit Scene">✏️</button>
-      <button v-if="!gMinigame.mode" class="hud-btn" @click="openLightMenu()" title="Edit Light">☀️</button>
-      <button v-if="!gMinigame.mode" class="hud-btn" @click="changeCamera()" title="Change Camera">🎥</button>    
+      <button v-if="!gMinigame.mode || gMinigame.mode === 5" class="hud-btn" @click="openLightMenu()" title="Edit Light">☀️</button>
+      <button v-if="!gMinigame.mode || gMinigame.mode === 5" class="hud-btn" @click="changeCamera()" title="Change Camera">🎥</button>    
     </div>
     
     <div v-if="gUI.shopMenu" class="modal-overlay">
@@ -107,7 +108,7 @@
         <button class="shop-btn" @click="changeGame(1, playerCount)" title="Spit Royale with AI">Spit Royale</button>
         <button v-if="isAuthenticated" class="shop-btn" @click="openLobbyMenu(2)" title="Spit Royale Online">Online Lobby</button>
         <button class="shop-btn" @click="changeGame(3, playerCount)" title="Alpaca Road">Alpaca Road</button>
-        <button class="shop-btn" @click="openLobbyMenu(4)" title="Alpaca Road Online">Online Lobby</button>
+        <button v-if="isAuthenticated" class="shop-btn" @click="openLobbyMenu(4)" title="Alpaca Road Online">Online Lobby</button>
       </div>
       <select v-model="playerCount" class="player-selector" title="Number of Players">
         <option :value="1">1 Player</option>
@@ -130,6 +131,20 @@
       </div>
     </div>
     <MultiplayerLobby v-if="gUI.lobbyMenu && gMinigame.mode === 4" />
+    <div v-if="gUI.lobbyMenu" class="modal-overlay">
+      <div class="shop-title">Visit A Friend's Farm
+        <div v-for="friend in gMinigame.lobby" :key="friend.id">
+          <button class="shop-btn" @click="visitFarm(friend.id, friend.username)">
+            {{ friend.username }}
+          </button>
+        </div>
+        <button class="close-btn" @click="closeLobbyMenu()" title="Close">✖️</button>
+      </div>
+    </div>
+    <div v-if="gMinigame.mode === 5" class="edit-mode">
+      <div class="shop-title"> Visiting {{ friendName }}'s Farm
+      </div>
+    </div>
     <div v-if="gUI.farmMenu" class="modal-overlay">
       <div class="shop-title">
         Upgrade Farm
@@ -313,7 +328,7 @@ import { useUIManager } from './core/useUIManager.js'
 import { watchChanges } from './core/watchChanges.js'
 import './game.css'
 import { updateAlpacaRoad } from './mini_games/alpacaRoad.js'
-import { changeGame } from './mini_games/init.js'
+import { changeGame, visitFarm, friendName } from './mini_games/init.js'
 import { getHearts } from './utils/uiHelpers.js'
 import { initWorld } from './world/initWorld.js'
 
